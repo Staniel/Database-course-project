fdcfc6b9d823e205d5fe0f80bea85345c6ab859d
LixinYao <yaolixinylx@gmail.com>
2015-04-02 12:21:21 -0400

    partially

--- moviedb/movie/models.py
+++ moviedb/movie/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+#single line use charField
+#multiple lines use textField
+class Movie(models.Model):
+	name = models.charField(max_length=50)
+	runtime = models.IntegerField(null=True, blank=True)
+    storyline = models.textField(null=True, blank=True)
+
+class Crew(models.Model):
+	name = models.charField(max_length=50)
+	description = models.textField(max_length=100)
+	crew_type = models.IntegerField()
+class Produce(models.Model):
+	mid = models.ForeignKey(Movie)
+	cid = models.ForeignKey(Crew)
+class Genre(models.Model):
+	name = models.charField(max_length=50)
+class BelongTo(models.Model):
+	mid = models.ForeignKey(Movie)
+	gid = models.ForeignKey(Genre)
+
+
