26a496e28ef0e1897a2988c48f0f98782facfc2c
LixinYao <yaolixinylx@gmail.com>
2015-04-02 15:14:29 -0400

    add url routing

--- moviedb/movie/admin.py
+++ moviedb/movie/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
-
+from movie.models import Movie, Crew, Produce, Genre, BelongTo, ReleaseInfo, Review, Watch, Favorite, Post, Topic, Comment
 # Register your models here.
+admin.site.register(Movie)
+admin.site.register(Crew)
+admin.site.register(Produce)
+admin.site.register(Genre)
+admin.site.register(BelongTo)
+admin.site.register(ReleaseInfo)
+admin.site.register(Review)
+admin.site.register(Watch)
+admin.site.register(Favorite)
+admin.site.register(Post)
+admin.site.register(Topic)
+admin.site.register(Comment)
+
+
+
+
+
+
+
+
+
+
---
+++ moviedb/movie/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import patterns, url
+from movie.views import placeholder
+
+urlpatterns = patterns('',
+    url(r'^login$', placeholder, name='login'),
+    url(r'^logout$', placeholder, name='logout'),
+    url(r'^register$', placeholder, name='register'),
+    url(r'^updateinfo$', placeholder, name='updateinfo'),
+    url(r'^$', placeholder, name='main'),
+    url(r'^person/(?P<personid>[0-9]+)/$', placeholder, name='person'),
+    url(r'^movie/(?P<movieid>[0-9]+)/$', placeholder, name='movie'),
+    url(r'^movie/watch/add/(?P<movieid>[0-9]+)/$', placeholder, name='watchmovie'),
+    url(r'^movie/watch/delete/(?P<movieid>[0-9]+)/$', placeholder, name='unwatchmovie'),
+    url(r'^movie/favorite/add/(?P<movieid>[0-9]+)/$', placeholder, name='favmovie'),
+    url(r'^movie/favorite/delete/(?P<movieid>[0-9]+)/$', placeholder, name='unfavmovie'),
+    url(r'^discuss/post/$', placeholder, name='listpost'),
+    url(r'^discuss/post/view/(?P<postid>[0-9]+)/$', placeholder, name='viewpost'),
+    url(r'^discuss/post/delete/(?P<postid>[0-9]+)/$', placeholder, name='deletepost'),
+    url(r'^discuss/post/add/$', placeholder, name='addpost'),
+    url(r'^discuss/comment/delete/(?P<commentid>[0-9]+)/$', placeholder, name='deletecomment'),
+    url(r'^discuss/comment/add/$', placeholder, name='addcomment'),
+
+)
