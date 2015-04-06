26bec431cf7d0fb8efafed33f43db53ebbf30db2
LixinYao <yaolixinylx@gmail.com>
2015-04-06 14:03:44 -0400

    add navbar

--- moviedb/movie/views/MainView.py
+++ moviedb/movie/views/MainView.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-@login_required
+# @login_required
 def main(request):
-	context = {'username': request.user.username}
+	context = {'username': "visitor"}
+	if request.user.username:
+		context = {'username': request.user.username, 'loggedin': True}
 	return render(request, 'movie/main.html', context)
